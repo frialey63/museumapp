bfe050172361b3116bbc13c99522febbf20fdd8e
Paul <Paul@mayapan>
2022-08-09 17:59:08 +0100

    implemented the aboutview

--- src/main/java/org/pjp/museum/ui/view/about/AboutView.java
+++ src/main/java/org/pjp/museum/ui/view/about/AboutView.java
@@ -1,37 +1,62 @@
 package org.pjp.museum.ui.view.about;
 
 import org.pjp.museum.ui.view.MainLayout;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("About")
 @Route(value = "about", layout = MainLayout.class)
-public class AboutView extends VerticalLayout {
+public class AboutView extends VerticalLayout implements AfterNavigationObserver {
 
-    private static final long serialVersionUID = -8229793812252460151L;
+    private static final long serialVersionUID = -4389648159491795815L;
+
+    private static final String APP_NAME = "Museum App version ";
+
+    @Value("${about.heading}")
+    private String aboutHeading;
+
+    @Value("${about.image}")
+    private String aboutImage;
+
+    @Value("${application.version}")
+    private String applicationVersion;
+
+    private final Image image = new Image("images/empty-plant.png", "logo");
+
+    private final H2 heading = new H2();
+
+    private final Paragraph appParagraph = new Paragraph();
 
     public AboutView() {
         super();
 
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        add(image, heading, appParagraph);
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
 
         getStyle().set("text-align", "center");
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent event) {
+        image.setSrc(aboutImage);
+        image.setWidth("200px");
+
+        heading.setText(aboutHeading);
+
+        appParagraph.setText(APP_NAME + applicationVersion);
     }
 
 }
