34c568aa95cc7bcbedf30379d87a38f252377242
Paul <Paul@mayapan>
2022-08-11 14:45:19 +0100

    updated intro text

--- src/main/java/org/pjp/museum/ui/view/intro/IntroductionView.java
+++ src/main/java/org/pjp/museum/ui/view/intro/IntroductionView.java
@@ -28,9 +28,9 @@ public class IntroductionView extends VerticalLayout implements AfterNavigationO
     private static final Logger LOGGER = LoggerFactory.getLogger(IntroductionView.class);
 
     private final String descriptionStr = """
-                Many of the exhibits have a QR Code. Scan the code using this app to access an audio description of the exhibit. If the scanner does not work then the exhibit can be identified by using a manually entered code number.
-                <br><br/>
-                For the best experience we recommend using headphones otherwise we respectfully request that the volume on your phone handset is minimised to avoid disturbance to other visitors.
+                Many of the exhibits in this museum have a QR Code. Scan the code using this app to access an audio description of the exhibit. If the scanner does not work then the exhibit can be identified by using a manually entered code number.
+                <br/><br/>
+                <em>For the best experience we recommend using headphones otherwise we respectfully request that the volume on your phone handset is minimised to avoid disturbance to other visitors.</em>
             """;
 
     public IntroductionView() {
