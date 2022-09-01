fb36d9e070f117e0dd610ae1874dfe1c627e0f67
Paul <Paul@mayapan>
2022-09-01 13:39:24 +0100

    updated to use the Html5Qrcode vaadin add-on

--- src/main/java/org/pjp/museum/ui/view/scan/ScannerView.java
+++ src/main/java/org/pjp/museum/ui/view/scan/ScannerView.java
@@ -1,10 +1,14 @@
 package org.pjp.museum.ui.view.scan;
 
-import org.pjp.museum.ui.component.html5qrcode.Html5Qrcode;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.pjp.museum.ui.view.MainLayout;
 import org.pjp.museum.ui.view.exhibit.ExhibitView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vaadin.addon.html5qrcode.Html5Qrcode;
+import org.vaadin.addon.html5qrcode.Html5Qrcode.Option;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -22,11 +26,17 @@ public class ScannerView extends VerticalLayout {
     public ScannerView() {
         super();
 
-        Html5Qrcode component = new Html5Qrcode(sc -> {
-            LOGGER.debug("uuid = {}", sc.getUuid());
+        Map<String, Object> options = new HashMap<>();
 
-            UI.getCurrent().navigate(ExhibitView.class, sc.getUuid());
-        });
+        options.put(Option.fps.name(), 10);
+        options.put(Option.qrbox.name(), 250);
+        options.put(Option.supportedScanTypes.name(), "[Html5QrcodeScanType.SCAN_TYPE_CAMERA]");
+
+        Html5Qrcode component = new Html5Qrcode(sc -> {
+            LOGGER.debug("uuid = {}", sc.getValue());
+
+            UI.getCurrent().navigate(ExhibitView.class, sc.getValue());
+        }, options);
 
         component.setWidthFull();
         component.setHeightFull();
